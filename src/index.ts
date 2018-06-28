895e8ac7bf32a53aef986610dab32bbc3a86a712
laopo001 <353272497@qq.com>
2018-06-29 01:29:15 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,23 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Friday, June 29th 2018, 12:01:28 am
+ * Last Modified: Friday, June 29th 2018, 1:29:05 am
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
+import * as capnp from 'capnp-ts';
+import { Date } from './schema/date.capnp';
 
-
+export function loadMessage(buffer: ArrayBuffer): Date {
+    const message = new capnp.Message(buffer);
+    return message.getRoot(Date);
+}
+const date = {
+    year: 1992,
+    month: 10,
+    day: 25
+}
+capnp.Message.toArrayBuffer(new capnp.Message())
 console.log(123)
+

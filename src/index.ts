f475af6fec3ae4a14c3c570aa3161df55ff10368
laopo001 <353272497@qq.com>
2018-08-15 10:22:30 +0800

    qaq

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,21 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Friday, June 29th 2018, 12:01:28 am
+ * Last Modified: Tuesday, August 14th 2018, 11:10:52 am
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
 
+type Fn<T=void> = (...args) => T
+type NullAble<T> = null | T;
 
 console.log(123)
+
+let a: null | Fn
+// a = function (a) { console.log(123) }
+let b: NullAble<string>
+
+a=null;
+
+function q(z: string) { }

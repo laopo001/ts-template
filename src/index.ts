52a32cf6ccbb1d08b70665fc4d7d0d44cca5448b
laopo001 <353272497@qq.com>
2018-08-23 19:30:37 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,22 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Friday, June 29th 2018, 12:01:28 am
+ * Last Modified: Thursday, August 23rd 2018, 7:06:46 pm
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
 
 
-console.log(123)
+import * as Handlebars from 'handlebars';
+
+var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+             "{{kids.length}} kids:</p>" +
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+var template = Handlebars.compile(source);
+ 
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+var result = template(data);
+
+console.log(result)

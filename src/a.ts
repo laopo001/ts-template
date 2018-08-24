d4ee5e430645743b39f77e5272c770097177549c
laopo001 <353272497@qq.com>
2018-08-24 19:47:05 +0800

    fix

---
+++ src/a.ts
@@ -0,0 +1,15 @@
+/**
+ * ProjectName: hypergl-demo
+ * FilePath: \src\a.ts
+ * Created Date: Wednesday, August 15th 2018, 10:26:39 am
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Wednesday, August 15th 2018, 10:27:09 am
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+export const add = (a, b) => a + b;
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,36 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Friday, June 29th 2018, 12:01:28 am
+ * Last Modified: Wednesday, August 15th 2018, 11:07:45 am
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
 
+import { add } from "./a";
+import * as e from "./assets/main.wasm";
+declare const WebAssembly;
+
+console.log(e)
+// fetch('./assets/main.wasm').then(response =>
+//     response.arrayBuffer()
+// ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+//     let instance = results.instance;
+//     let innerText = instance.exports.add_one(41);
+//     console.log(innerText)
+//     export const a = 123;
+// }).catch(console.error);
+type Fn<T=void> = (...args) => T
+type NullAble<T> = null | T;
 
 console.log(123)
+
+let a: null | Fn
+// a = function (a) { console.log(123) }
+let b: NullAble<string>
+
+a = null;
+
+function q(z: string) { }
+
+add(1, 2)

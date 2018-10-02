0296f0b1c8e61d5dc47471acd7f2660651095370
liaodh <liaodh@jpush.com>
2018-10-03 00:38:10 +0800

    fix

--- build/index.js
+++ build/index.js
@@ -275,7 +275,7 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ./index */ "./src/index.js"))
+__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.t.bind(null, /*! ./index */ "./src/index.ts", 7))
   .catch(e => console.error("Error importing `index.js`:", e));
 
 

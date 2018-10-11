70fe432a7bb147b6c397fb3bd53d7f114e7f0c59
liaodh <liaodh@jpush.com>
2018-10-11 16:26:28 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = function (env, webpackConfig) {
     let res = {
         //页面入口文件配置
         entry: {
-            index: `./src/bootstrap`
+            index: `./src/index`
         },
         //入口文件输出配置
         output: {

daccf69b2dc82ddb3dfd85ab5fb130fbee06618c
liaodh <liaodh@jpush.com>
2018-10-11 16:26:35 +0800

    fix

---
+++ src/Hello.tsx
@@ -0,0 +1,18 @@
+/**
+ * ProjectName: hypergl-demo
+ * FilePath: \src\Hello.ts
+ * Created Date: Thursday, October 11th 2018, 3:24:45 pm
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Thursday, October 11th 2018, 3:30:43 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+
+
+import * as React from 'react';
+import { Component } from 'react';
+
+export default ({ name }) => <h1>Hello { name } !</h1>;
---
+++ src/index.tsx
@@ -0,0 +1,44 @@
+/**
+ * File: c:\Users\35327\Githubs\ts-template\src\index.ts
+ * Project: c:\Users\35327\Githubs\ts-template
+ * Created Date: Friday, June 29th 2018, 12:01:19 am
+ * @author: liaodh
+ * @summary: short description for the file
+ * -----
+ * Last Modified: Thursday, October 11th 2018, 3:24:48 pm
+ * Modified By: liaodh
+ * -----
+ * Copyright (c) 2018 jiguang
+ */
+import * as React from 'react';
+import { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+// import './style.css';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));

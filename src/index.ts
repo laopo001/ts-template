d21e0aad21ef0b2950f009c19082e834cbd7882b
liaodh <liaodh@jiguang.cn>
2019-04-01 19:17:47 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,22 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Monday, April 1st 2019, 5:04:54 pm
+ * Last Modified: Monday, April 1st 2019, 7:17:23 pm
  * Modified By: liaodh
  * -----
  * Copyright (c) 2018 jiguang
  */
 
 
+import { MacroTask } from "./macro_task";
+MacroTask.run(function () {
+    return 111;
+}).then(x => {
+})
+
+let t = new MacroTask(function (resolve, reject) {
+    resolve(456);
+}).then((res) => {
+    console.log(res);
+})
 console.log(123)
